3ba47b06f29e0964f9051a09d21ca726f63c19ec
aleksis <ipa21.a.velvelis@vtdt.edu.lv>
2023-11-01 14:18:08 +0200

    Commited PirmaisJaut, OtraisJaut components

---
+++ src/OtraisJaut.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function OtraisJaut(){
+    return (
+        <article className="textWhite">
+            <p>Kad un kur izmanto ER diagrammas?</p>
+            <p>ER (Entity-Relation) diagrammas lieto, lai noradītu relācijas starp tabulām un attēlotu tās vienkāršā un nepārprotamā formā.</p>
+        </article>
+      );
+}
+export default OtraisJaut;
