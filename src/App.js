4ba4dde39cc2c8fb849b3f8da7bccf69fd172c0c
Wolfijs <ralfs.valters.zamuelis@gmail.com>
2023-11-01 14:25:32 +0200

    Ralfs

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Ralfs from './components/Ralfs';
+function App() {
+  return (
+    
+    <main className="App-header">
+         <Ralfs/>
+    </main>
+    
+  );
+}
+
+export default App;
